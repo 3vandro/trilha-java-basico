c6c64c0ebf6d33aaa577aa5f73d3348d8d2fd885
Evandro Sacramento <evandro-dev@outlook.com>
2023-10-12 22:37:51 -0300

    atualizações

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,46 @@
+public class Operadores {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+// String nome = "Evandro";
+// int idade = 34;
+// double peso = 68.5;
+// char sexo = 'M';
+// boolean doadorOrgao = false;
+// Date dataNascimento = new Date()
+
+        String nomeCompleto = "Liguagem " + "Java";
+        System.out.println(nomeCompleto);
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1" +(1+1+1);
+        System.out.println(concatenacao);
+
+        // Operadores 3
+
+        int numero = 5;
+        System.out.println(-numero);
+        System.out.println(numero);
+        numero= -numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        numero++;
+        System.out.println(numero);
+
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
+
+
+        
+
+    }
+}
